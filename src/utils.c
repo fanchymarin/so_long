92b232fcb7a29ddac761e0cee1eac5dd193ca531
fmarin-p <franmp97@outlook.com>
2022-05-14 11:52:46 +0200

    rand function added

--- src/utils.c
+++ src/utils.c
@@ -31,3 +31,12 @@ void	free_dp(char **line)
 		free(line[i++]);
 	free(line);
 }
+
+int rand(void)
+{
+	static int next = 1;
+       
+	next = next * 1103515245 + 12345;
+	return (unsigned int)(next/65536) % 32768;
+}
+
