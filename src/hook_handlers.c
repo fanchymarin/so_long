324a5fa264f0e00ff29f5d8ca655d5f286881d55
Francisco Marín Pérez <fmarin-p@student.42madrid.com>
2022-05-25 14:14:20 +0200

    Speed rate not controlled on Mac

--- src/hook_handlers.c
+++ src/hook_handlers.c
@@ -6,7 +6,7 @@
 /*   By: fmarin-p <fmarin-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 17:34:26 by fmarin-p          #+#    #+#             */
-/*   Updated: 2022/05/23 21:35:01 by fmarin-p         ###   ########.fr       */
+/*   Updated: 2022/05/25 14:13:15 by fmarin-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	animate_c(t_mlx *mlx)
 {
 	static t_list	*collect = 0;
-	static int		speed = 6000;
+	static int		speed = 10;
 	int				i;
 
 	if (!collect)
@@ -34,13 +34,14 @@ void	animate_c(t_mlx *mlx)
 	if (!speed--)
 	{
 		collect = collect->next;
-		speed = 6000;
+		speed = 10;
 	}
 }
 
 void	animate_e(t_mlx *mlx)
 {
 	static t_list	*exit = 0;
+	static int		speed = 5;
 	int				i;
 
 	if (!exit)
@@ -55,18 +56,21 @@ void	animate_e(t_mlx *mlx)
 			exit->content, mlx->e_pos[i].x * PSIZE, mlx->e_pos[i].y * PSIZE);
 		i++;
 	}
-	exit = exit->next;
+	if (!speed--)
+	{
+		exit = exit->next;
+		speed = 5;
+	}
 }
 
 void	move_player(t_mlx *mlx, t_list **sprite, t_pos pos, t_pos new_pos)
 {
 	int			pixel_mov;
-	static int	speed = 500;
 	t_list		*image;
 
-	pixel_mov = 1;
+	pixel_mov = 0;
 	image = *sprite;
-	while (pixel_mov <= PSIZE)
+	while (pixel_mov <= PSIZE && pixel_mov >= (PSIZE * -1))
 	{
 		if (!image)
 			image = *sprite;
@@ -74,19 +78,18 @@ void	move_player(t_mlx *mlx, t_list **sprite, t_pos pos, t_pos new_pos)
 			ret_f(mlx, pos.x, pos.y), pos.x * PSIZE, pos.y * PSIZE);
 		mlx_put_image_to_window(mlx->mlx, mlx->mlx_win,
 			ret_f(mlx, pos.x + new_pos.x, pos.y + new_pos.y),
-			pos.x + new_pos.x * PSIZE, pos.y + new_pos.y * PSIZE);
+			(pos.x + new_pos.x) * PSIZE, (pos.y + new_pos.y) * PSIZE);	
 		if (new_pos.x)
 			mlx_put_image_to_window(mlx->mlx, mlx->mlx_win,
 				image->content, (pos.x * PSIZE) + pixel_mov, pos.y * PSIZE);
 		else
 			mlx_put_image_to_window(mlx->mlx, mlx->mlx_win,
 				image->content, pos.x * PSIZE, (pos.y * PSIZE) + pixel_mov);
-		if (!speed--)
-		{
-			pixel_mov++;
-			image = image->next;
-			speed = 500;
-		}
+		if (new_pos.x == 1 || new_pos.y == 1)
+			pixel_mov += 2;
+		else
+			pixel_mov -= 2;
+		image = image->next;
 	}
 }
 
@@ -110,9 +113,6 @@ void	stat_player(t_mlx *mlx, t_list **sprite, t_pos new_pos)
 			move_player(mlx, sprite, *mlx->p_pos, new_pos);
 			mlx->p_pos[i].x += new_pos.x;
 			mlx->p_pos[i].y += new_pos.y;
-			mlx_put_image_to_window(mlx->mlx, mlx->mlx_win,
-				(**sprite).content, mlx->p_pos[i].x * PSIZE,
-				mlx->p_pos[i].y * PSIZE);
 		}
 		++i;
 	}	
@@ -123,24 +123,12 @@ int	key_hook(int key, t_mlx *mlx)
 	t_pos	new_pos;
 
 	if (key == KEY_S)
-	{
-		new_pos = (t_pos){0, 1};
-		stat_player(mlx, mlx->player[0], new_pos);
-	}
+		stat_player(mlx, mlx->player[0], new_pos = (t_pos){0, 1});
 	else if (key == KEY_A)
-	{
-		new_pos = (t_pos){-1, 0};
-		stat_player(mlx, mlx->player[1], new_pos);
-	}
+		stat_player(mlx, mlx->player[1], new_pos = (t_pos){-1, 0});
 	else if (key == KEY_W)
-	{
-		new_pos = (t_pos){0, -1};
-		stat_player(mlx, mlx->player[2], new_pos);
-	}
+		stat_player(mlx, mlx->player[2], new_pos = (t_pos){0, -1});
 	else if (key == KEY_D)
-	{
-		new_pos = (t_pos){1, 0};
-		stat_player(mlx, mlx->player[3], new_pos);
-	}
+		stat_player(mlx, mlx->player[3], new_pos = (t_pos){1, 0});
 	return (0);
 }
